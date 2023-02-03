db3de18616411d87e748937ad2a74f5818e3e467
Saki <sbasken@gmail.com>
2023-02-03 14:46:01 -0800

    commit tree 5c8a0cf1df7219c2b432c22a3a7ea07d9dac9132

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })  
+    })
+    .then(res => res.json())
+    .then(userInfo => {
+        const script = document.querySelector("script");
+        const h1 = document.createElement("h1");
+        h1.innerHTML = userInfo.id;
+        script.appendChild(h1);
+    })
+}
+
+
